66a2471dac3b5ffc2b7c8c9ca600a543a189d53f
Rok Garbas <rok@garbas.si>
2010-10-02 03:04:38 +0200

    hopefully last reorganisation of code (defining public and private parts)

--- src/jquery.recurrenceinput.js
+++ src/jquery.recurrenceinput.js
@@ -11,96 +11,174 @@
      * TODO:
      *
      */
-    var default_conf = {}
+    var default_conf = {};
 
     // private
 
     // helper translation function
     function _(str, args) { 
-        return $.i18n('jquery-recurrenceinput-js', str, args); 
+        return $.i18n('jquery-recurrenceinput', str, args); 
     }
 
     function Recurrenceinput (textarea, conf) {
 
-        var widget_str = '\
-            <div class="recurrenceinput">\
-              <div class="ruleset"></div>\
-              <div class="buttons">\
-                 <a href="#" class="add_rrule">'+_('Add RRULE')+'</a>\
-                 <a href="#" class="add_exrule">'+_('Add EXRULE')+'</a>\
-                 <a href="#" class="add_rdate">'+_('Add RDATE')+'</a>\
-                 <a href="#" class="add_exdate">'+_('Add EXDATE')+'</a>\
-              </div>\
-              <div class="clear"><!-- --></div>\
-            </div>'; 
+        var widget_str = '<div class="recurrenceinput"><div class="ruleset"></div><div class="buttons"><a href="#" class="add_rrule">'+_('Add RRULE')+'</a><a href="#" class="add_exrule">'+_('Add EXRULE')+'</a><a href="#" class="add_rdate">'+_('Add RDATE')+'</a><a href="#" class="add_exdate">'+_('Add EXDATE')+'</a></div><div style="clear:both;"><!-- --></div></div>'; 
+        var rule_str = '<div class="rule RULE_CLASS"><form><a href="#" class="remove">'+_('Remove RRULE')+'</a><div style="clear:both;"><!-- --></div><ul class="freq" style="list-style:none; margin: 0; padding: 0 1em 0 0; display: block; float: left;"><li style="margin: 0;"><input type="radio" name="freq" value="DAILY" /><label>'+_('Daily')+'</label></li><li style="margin: 0;"><input type="radio" name="freq" value="WEEKLY" /><label>'+_('Weekly')+'</label></li><li style="margin: 0;"><input type="radio" name="freq" value="MONTHLY" /><label>'+_('Monthly')+'</label></li><li style="margin: 0;"><input type="radio" name="freq" value="YEARLY" /><label>'+_('Yearly')+'</label></li></ul><div class="freq-options"><div class="daily" style="margin: 0 0 0 1em;">DAILY OPTIONS</div><div class="weekly" style="margin: 0 0 0 1em;">WEEKLY OPTIONS</div><div class="monthly" style="margin: 0 0 0 1em;">MONTHLY OPTIONS</div><div class="yearly" style="margin: 0 0 0 1em;">YEARLY OPTIONS</div></div><div style="clear:both;"><!-- --></div></form></div>';
 
-        var rrule_str = '\
-            <div class="rrule">\
-              <form>\
-                <a href="#" class="remove">'+_('Remove RRULE')+'</a>\
-                <div class="clear"><!-- --></div>\
-                <ul class="freq">\
-                  <li>\
-                    <input type="radio" name="freq" value="DAILY" />\
-                    <label>'+_('Daily')+'</label>\
-                  </li>\
-                  <li>\
-                    <input type="radio" name="freq" value="WEEKLY" />\
-                    <label>'+_('Weekly')+'</label>\
-                  </li>\
-                  <li>\
-                    <input type="radio" name="freq" value="MONTHLY" />\
-                    <label>'+_('Monthly')+'</label>\
-                  </li>\
-                  <li>\
-                    <input type="radio" name="freq" value="YEARLY" />\
-                    <label>'+_('Yearly')+'</label>\
-                  </li>\
-                </ul>\
-                <div class="freq-options">\
-                  <div class="daily">DAILY OPTIONS</div>\
-                  <div class="weekly">WEEKLY OPTIONS</div>\
-                  <div class="monthly">MONTHLY OPTIONS</div>\
-                  <div class="yearly">YEARLY OPTIONS</div>\
-                </div>\
-                <div class="clear"><!-- --></div>\
-              </form>\
-            </div>';
+        var self = this;
+        var widget = $(widget_str);
+        var widget_ruleset = widget.find('.ruleset');
 
-        var self = this,
-            textarea = textarea;
-            widget = $(widget_str);
-            ruleset = widget.find('.ruleset');
-            form = detect_form(textarea);
 
-        // on form submit we write 
-        form.submit(function(e) {
-            var ruleset_str = '';
-            widget.find('.ruleset > div').each(function () {
-                $this = $(this);
-                if ($this.hasClass('rrule')) {
-                    ruleset_str += parse_rrule($this)+'\n';
-                }
+
+        /*
+         * Initial steps to activate widget
+         */
+
+        // initialize
+        // TODO:
+
+        // add actions to widget buttons
+        widget.find('.buttons > a')
+            .unbind('click')
+            .click(function () {
+                // TODO: add rrule, exdate, rdate or exdate 
+                add_rule();
             });
-            textarea.val(ruleset_str);
-            widget.html('');
+
+
+
+        function add_rule (rule_class, data) {
+            var rule = $(rule_str.replace('RULE_CLASS', rule_class));
+
+            // hide options of frequencies
+            rule.find('.freq-options > div').css('display', 'none');
+
+            // make label of freq option active for selection
+            rule.find('.freq label').unbind("click").click(function () {
+                var input = $(this).parent().find('input[name=freq]');
+                input.click(); input.change();
+            });
+
+            // select 
+            rule.find('.freq input[name=freq]').unbind("change").change(function() {
+                $this = $(this);
+                rule.find('.freq input[name=freq]').attr('class', '');
+                $this.attr('class', 'active')
+                rule.find('.freq-options > div').css('display', 'none');
+                rule.find('.freq-options .' + $this.val().toLowerCase())
+                        .css('display', 'block')
+                        .css('margin-left', +$this.parent().parent().width() +
+                                            2*(+($this.parent().parent().css('font-size')
+                                                .replace('px', '')
+                                                .replace('em', ''))));
+            });
+
+            // remove rrule action
+            rule.find('a.remove').unbind("click").click(function () {
+                $(this).parent().remove();
+            });
+
+            // append rrule to ruleset
+            widget_ruleset.append(rule);
+
+        }
+
+
+
+        /*
+         * Parsing RDF2554 from widget
+         */
+
+        // method for parsing rules (rrule and exrule)
+        function parse_rule(el) {
+            var str_ = '';
+            str_ += 'FREQ='+el.find('input[class=active]').val()
+            // TODO: parse other options
+            return str_;
+        }
+
+        // function for parsing dates (rdate and exdate)
+        function parse_rule(el) {
+            var str_ = '';
+            // TODO: parse other options
+            return str_;
+        }
+
+
+
+        /*
+         * Public API of Recurrenceinput
+         */
+
+        $.extend(self, {
+            widget: widget,
+            widget_ruleset: widget_ruleset,
+            parse_rrule: function (el) { return 'RRULE: '+parse_rule(el) },
+            parse_exrule: function (el) { return 'EXRULE: '+parse_rule(el) },
+            parse_rdate: function (el) { return 'RDATE: '+parse_date(el) },
+            parse_exdate: function (el) { return 'EXDATE: '+parse_date(el) }
         });
 
-        // add action to buttons
-        // TODO: add action to other buttons
-        widget.find('.buttons a.add_rrule').unbind('click').click(function () {
-            add_rrule();
-        })
+    }
 
 
-        // HELPING FUNCTIONS
 
-        function parse_rrule (rule) {
-            var rule_str = 'RRULE:';
-            rule_str += 'FREQ='+rule.find('input[class=active]').val()
-            // TODO: parse other options
-            return rule_str;
-        }
+    /*
+     * jQuery plugin implementation
+     */
+
+    $.fn.recurrenceinput = function(conf) {
+
+        // already installed
+        if (this.data("recurrenceinput")) { return this; } 
+
+        // apply this for every textarea we can match
+        this.each(function() {
+            if (this.tagName == 'TEXTAREA') {
+
+                var textarea = $(this);
+                var form = detect_form(textarea);
+                var recurrenceinput = new Recurrenceinput($.extend(true, {}, default_conf, conf));
+
+                // hide textarea
+                textarea.css('display', 'none');
+
+                // on form submit we write to textarea
+                form.submit(function(e) {
+
+                    // create string for rule widget
+                    var ruleset_str = '';
+                    recurrenceinput.widget_ruleset.each(function () {
+                        var el = $(this);
+                        if (el.hasClass('rrule')) {
+                            ruleset_str += recurrenceinput.parse_rrule(el)+'\n';
+                        } else if (el.hasClass('exrule')) {
+                            ruleset_str += recurrenceinput.parse_exrule(el)+'\n';
+                        } else if (el.hasClass('rdate')) {
+                            ruleset_str += parse_rdate(el)+'\n';
+                        } else if (el.hasClass('exdate')) {
+                            ruleset_str += parse_exdate(el)+'\n';
+                        }
+                    });
+
+                    // insert string generated form above to textarea
+                    textarea.val(ruleset_str);
+
+                    // remove widget
+                    recurrenceinput.widget.remove();
+                });
+
+                // insert recurrance widget right after textarea 
+                textarea.after(recurrenceinput.widget)
+            };
+        });
+
+
+
+        /*
+         * HELPING FUNCTIONS
+         */
 
         // FIXME: maybe there is a better way to detect form
         function detect_form (textarea) {
@@ -113,87 +191,6 @@
             }
             return form
         }
-
-        // adds rrule to widget
-        function add_rrule (data) {
-            var rrule = $(rrule_str);
-
-            // hide options of frequencies
-            rrule.find('.freq-options > div').css('display', 'none');
-
-            // make label of freq option active for selection
-            rrule.find('.freq label').unbind("click").click(function () {
-                var input = $(this).parent().find('input[name=freq]');
-                input.click(); input.change();
-            });
-
-            // select 
-            rrule.find('.freq input[name=freq]').unbind("change").change(function() {
-                $this = $(this);
-                rrule.find('.freq input[name=freq]').attr('class', '');
-                $this.attr('class', 'active')
-                rrule.find('.freq-options > div').css('display', 'none');
-                rrule.find('.freq-options .' + $this.val().toLowerCase())
-                        .css('display', 'block')
-                        .css('margin-left', +$this.parent().parent().width() +
-                                            2*(+($this.parent().parent().css('font-size')
-                                                .replace('px', '')
-                                                .replace('em', ''))));
-            });
-
-            // remove rrule action
-            rrule.find('a.remove').unbind("click").click(function () {
-                $(this).parent().remove();
-            });
-
-            // append rrule to ruleset
-            ruleset.append(rrule);
-
-        }
-
-        $.extend(self, {
-            init: function (data) {
-                // add empty rrule widget
-                if (data === undefined || data == "") {
-                    add_rrule();
-                // widget
-                } else {
-                    alert('TODO: Need to parse and polute widget!');
-                }
-            },
-            getWidget: function () {
-                return widget;
-            }
-        });
-
-    }
-
-    // jQuery plugin implementation
-    $.fn.recurrenceinput = function(conf) {
-
-        // already installed
-        if (this.data("recurrenceinput")) { return this; } 
-
-        // extend configuration with globals
-        conf = $.extend(true, {}, default_conf, conf);        
-
-        this.each(function() {
-            if (this.tagName == 'TEXTAREA') {
-                $this = $(this);
-                var d_recurr = new Recurrenceinput($this, $.extend(true, {}, conf));
-
-                // hide textarea we are submiting
-                $this.css('display', 'none');
-
-                // initialize recurrance widget and polute with data
-                d_recurr.init($this.val())
-
-                // insert recurrance widget after textarea 
-                $this.after(d_recurr.getWidget())
-            };
-        });        
-        
-    };    
-
+    };
 
 })(jQuery);
