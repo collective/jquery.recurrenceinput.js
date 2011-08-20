f9fe36cc659e63e4e6ba5139a047b793d0f963e7
Lennart Regebro <regebro@gmail.com>
2011-08-20 18:10:07 +0200

    The form now uses the recurrence templating system.

--- src/jquery.recurrenceinput.form.js
+++ src/jquery.recurrenceinput.form.js
@@ -24,7 +24,7 @@
                     name="${field.weekly_interval_name}" />
                 <ul>
             </div>
-            <div id="${klass.weekly_weekdays}" class="recurrenceinput_field">
+            <div id="${field.weekly_weekdays_name}" class="recurrenceinput_field">
                 {{each i18n.weekdays}}
                     <input type="checkbox"
                         name="${field.weekly_weekdays_name}_${weekdays[$index]}"
@@ -34,135 +34,129 @@
                 </ul>
             </div>
     
-                <div class="${klass.freq_monthly} recurrenceinput_field">
-                    <ul>
-                        <li>
-                            <input
-                                type="radio"
-                                value="${field.monthly_day_of_month_value}"
-                                name="${field.monthly_type_name}" />
-                            <span>${i18n.monthly_day_of_month}</span>
-                            <select name="${field.monthly_day_of_month_day_name}">
-                            {{each [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
-                                    18,19,20,21,22,23,24,25,26,27,28,29,30,31]}}
-                                <option value="${$value}">${$value}</option>
-                            {{/each}}
-                            </select>
-                            <input type="text" size="2"
-                                value="${field.monthly_day_of_month_interval_value}" 
-                                name="${field.monthly_day_of_month_interval_name}"/>
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                value="${field.monthly_weekday_of_month_value}"
-                                name="${field.monthly_type_name}" />
-                            <span>${i18n.monthly_weekday_of_month}</span>
-                            <!-- TODO: could it be multiselect? -->
-                            <select name="${field.monthly_weekday_of_month_index_name}">
-                            {{each i18n.order_indexes}}
-                                <option value="${order_indexes[$index]}">${$value}</option>
-                            {{/each}}
-                            </select>
-                            <select name="${field.monthly_weekday_of_month_name}">
-                            {{each i18n.weekdays}}
-                                <option value="${weekdays[$index]}">${$value}</option>
-                            {{/each}}
-                                <option value="${field.yearly_weekday_of_month_weekday_value}">
-                                    ${i18n.yearly_weekday_of_month_weekday}</option>
-                                <option value="${field.yearly_weekday_of_month_weekend_day_value}">
-                                    ${i18n.yearly_weekday_of_month_weekend_day}</option>
-                            </select>
-                            </select>
-                            <input type="text" size="2"
-                                value="${field.monthly_weekday_of_month_interval_value}"
-                                name="${field.monthly_weekday_of_month_interval_name}" />
-                        </li>
-                    </ul>
+            <div id="${field.monthly_options_name}" class="recurrenceinput_field">
+                <div>
+                    <input
+                        type="radio"
+                        value="${field.monthly_day_of_month_value}"
+                        name="${field.monthly_type_name}" />
+                    <span>${i18n.monthly_day_of_month}</span>
+                    <select name="${field.monthly_day_of_month_day_name}">
+                    {{each [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
+                            18,19,20,21,22,23,24,25,26,27,28,29,30,31]}}
+                        <option value="${$value}">${$value}</option>
+                    {{/each}}
+                    </select>
+                    <input type="text" size="2"
+                        value="${field.monthly_day_of_month_interval_value}" 
+                        name="${field.monthly_day_of_month_interval_name}"/>
                 </div>
+                <div>
+                    <input
+                        type="radio"
+                        value="${field.monthly_weekday_of_month_value}"
+                        name="${field.monthly_type_name}" />
+                    <span>${i18n.monthly_weekday_of_month}</span>
+                    <!-- TODO: could it be multiselect? -->
+                    <select name="${field.monthly_weekday_of_month_index_name}">
+                    {{each i18n.order_indexes}}
+                        <option value="${order_indexes[$index]}">${$value}</option>
+                    {{/each}}
+                    </select>
+                    <select name="${field.monthly_weekday_of_month_name}">
+                    {{each i18n.weekdays}}
+                        <option value="${weekdays[$index]}">${$value}</option>
+                    {{/each}}
+                        <option value="${field.yearly_weekday_of_month_weekday_value}">
+                            ${i18n.yearly_weekday_of_month_weekday}</option>
+                        <option value="${field.yearly_weekday_of_month_weekend_day_value}">
+                            ${i18n.yearly_weekday_of_month_weekend_day}</option>
+                    </select>
+                    </select>
+                    <input type="text" size="2"
+                        value="${field.monthly_weekday_of_month_interval_value}"
+                        name="${field.monthly_weekday_of_month_interval_name}" />
+                </div>
+            </div>
     
-                <div class="${klass.freq_yearly} recurrenceinput_field">
-                    <ul>
-                        <li>
-                            <input
-                                type="radio"
-                                value="${field.yearly_day_of_month_value}"
-                                name="${field.yearly_type_name}" />
-                            <span>${i18n.yearly_day_of_month}</span>
-                            <select name="${field.yearly_day_of_month_name}">
-                            {{each months}}
-                                <option value="${$index+1}">${$value}</option>
-                            {{/each}}
-                            </select>
-                            <select name="${field.yearly_day_of_month_index_name}">
-                            {{each [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
-                                    18,19,20,21,22,23,24,25,26,27,28,29,30,31]}}
-                                <option value="${$value}">${$value}</option>
-                            {{/each}}
-                            </select>
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                value="${field.yearly_weekday_of_month_value}"
-                                name="${field.yearly_type_name}" />
-                            <span>${i18n.yearly_weekday_of_month}</span>
-                            <select name="${field.yearly_weekday_of_month_index_name}">
-                            {{each i18n.order_indexes}}
-                                <option value="${order_indexes[$index]}">${$value}</option>
-                            {{/each}}
-                            </select>
-                            <select name="${field.yearly_weekday_of_month_name}">
-                            {{each i18n.weekdays}}
-                                <option value="${weekdays[$index]}">${$value}</option>
-                            {{/each}}
-                                <option value="${field.yearly_weekday_of_month_weekday_value}">
-                                    ${i18n.yearly_weekday_of_month_weekday}</option>
-                                <option value="${field.yearly_weekday_of_month_weekend_day_value}">
-                                    ${i18n.yearly_weekday_of_month_weekend_day}</option>
-                            </select>
-                            <select name="${field.yearly_weekday_of_month_month_name}">
-                            {{each months}}
-                                <option value="${$index+1}">${$value}</option>
-                            {{/each}}
-                            </select>
-                        </li>
-                    </ul>
+            <div id="${field.yearly_options_name}" class="recurrenceinput_field">
+                <div>
+                    <input
+                        type="radio"
+                        value="${field.yearly_day_of_month_value}"
+                        name="${field.yearly_type_name}" />
+                    <span>${i18n.yearly_day_of_month}</span>
+                    <select name="${field.yearly_day_of_month_name}">
+                    {{each months}}
+                        <option value="${$index+1}">${$value}</option>
+                    {{/each}}
+                    </select>
+                    <select name="${field.yearly_day_of_month_index_name}">
+                    {{each [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,
+                            18,19,20,21,22,23,24,25,26,27,28,29,30,31]}}
+                        <option value="${$value}">${$value}</option>
+                    {{/each}}
+                    </select>
                 </div>
+                <div>
+                    <input
+                        type="radio"
+                        value="${field.yearly_weekday_of_month_value}"
+                        name="${field.yearly_type_name}" />
+                    <span>${i18n.yearly_weekday_of_month}</span>
+                    <select name="${field.yearly_weekday_of_month_index_name}">
+                    {{each i18n.order_indexes}}
+                        <option value="${order_indexes[$index]}">${$value}</option>
+                    {{/each}}
+                    </select>
+                    <select name="${field.yearly_weekday_of_month_name}">
+                    {{each i18n.weekdays}}
+                        <option value="${weekdays[$index]}">${$value}</option>
+                    {{/each}}
+                        <option value="${field.yearly_weekday_of_month_weekday_value}">
+                            ${i18n.yearly_weekday_of_month_weekday}</option>
+                        <option value="${field.yearly_weekday_of_month_weekend_day_value}">
+                            ${i18n.yearly_weekday_of_month_weekend_day}</option>
+                    </select>
+                    <select name="${field.yearly_weekday_of_month_month_name}">
+                    {{each months}}
+                        <option value="${$index+1}">${$value}</option>
+                    {{/each}}
+                    </select>
+                </div>
+            </div>
                 
-                <div class="${klass.range} recurrenceinput_field">
-                    <label>${i18n.range_label}</label>
-                    <ul>
-                        <li>
-                            <input
-                                type="radio"
-                                value="${field.range_no_end}"
-                                name="${field.range_type_name}" />
-                            <label>${i18n.range_no_end_label}</label>
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                value="${field.range_by_ocurrences}"
-                                name="${field.range_type_name}" />
-                            <label>${i18n.range_by_occurences_label}</label>
-                            <input
-                                type="text" size="3"
-                                value="${field.range_by_ocurrences_value}"
-                                name="${field.range_by_ocurrences_value_name}" />
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                value="${field.range_by_end_date}"
-                                name="${field.range_type_name}" />
-                            <label>${i18n.range_by_end_date_label}</label>
-                            <input
-                                type="date" 
-                                name="${field.range_by_end_date_calendar_name}" />
-                        </li>
-                    </ul>
+            <div id="${field.range_options_name}" class="recurrenceinput_field">
+                <label>${i18n.range_label}</label>
+                <div>
+                    <input
+                        type="radio"
+                        value="${field.range_no_end}"
+                        name="${field.range_type_name}" />
+                    <label>${i18n.range_no_end_label}</label>
                 </div>
+                <div>
+                    <input
+                        type="radio"
+                        value="${field.range_by_ocurrences}"
+                        name="${field.range_type_name}" />
+                    <label>${i18n.range_by_occurences_label}</label>
+                    <input
+                        type="text" size="3"
+                        value="${field.range_by_ocurrences_value}"
+                        name="${field.range_by_ocurrences_value_name}" />
+                </div>
+                <div>
+                    <input
+                        type="radio"
+                        value="${field.range_by_end_date}"
+                        name="${field.range_type_name}" />
+                    <label>${i18n.range_by_end_date_label}</label>
+                    <input
+                        type="date" 
+                        name="${field.range_by_end_date_calendar_name}" />
+                </div>
+            </div>
         
             <div class="${klass.clear}"><!-- --></div>
     
